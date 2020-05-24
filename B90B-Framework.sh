c6001d3dd0d74751131aa590754df11cf025b88b
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-05-24 22:11:52 +0200

    Create B90B-Framework.sh

---
+++ B90B-Framework.sh
@@ -0,0 +1,17 @@
+function tmain() {
+LEH=$(zenity --question --text="Do you want to run B9OB?" --title="B9OB")
+if [ $LEH == "Yes" ];
+echo "B9OB"
+touch TROJAN.RANSOM.TOYOR.TROJPIF.py
+echo "nullos = num = input('B9OB>>>>')"> TROJAN.RANSOM.TOYOR.TROJPIF.py
+echo "nully = ['help','jay','shay','vhay']"> TROJAN.RANSOM.TOYOR.TROJPIF.py
+echo "if nullos == nully:"> TROJAN.RANSOM.TOYOR.TROJPIF.py
+echo "print nullos ':,POOOO'"> TROJAN.RANSOM.TOYOR.TROJPIF.py
+echo "else:" > TROJAN.RANSOM.TOYOR.TROJPIF.py
+echo "print nullos,'not found'"> TROJAN.RANSOM.TOYOR.TROJPIF.py
+python TROJAN.RANSOM.TOYOR.TROJPIF.py
+else
+exit 0
+
+
+

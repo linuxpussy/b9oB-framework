7f5ab088323bdacf1099b6286f55117978993bdc
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-05-24 22:14:09 +0200

    Update B90B-Framework.sh

--- B90B-Framework.sh
+++ B90B-Framework.sh
@@ -3,7 +3,7 @@ LEH=$(zenity --question --text="Do you want to run B9OB?" --title="B9OB")
 if [ $LEH == "Yes" ];
 echo "B9OB"
 touch TROJAN.RANSOM.TOYOR.TROJPIF.py
-echo "nullos = num = input('B9OB>>>>')"> TROJAN.RANSOM.TOYOR.TROJPIF.py
+echo "nullos = num = input('bahi.main.bstart')"> TROJAN.RANSOM.TOYOR.TROJPIF.py
 echo "nully = ['help','jay','shay','vhay']"> TROJAN.RANSOM.TOYOR.TROJPIF.py
 echo "if nullos == nully:"> TROJAN.RANSOM.TOYOR.TROJPIF.py
 echo "print nullos ':,POOOO'"> TROJAN.RANSOM.TOYOR.TROJPIF.py
